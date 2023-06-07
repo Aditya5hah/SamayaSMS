4803967a0f59256549f3c23a1ae0471c86ff47fd
Aditya Shah <adityashahnepal12@gmail.com>
2023-06-07 18:52:32 +0545

    Third commit with animation and map embedded

--- main.js
+++ main.js
@@ -31,3 +31,34 @@ function scrollToTop() {
         behavior: "smooth"
     });
 }
+
+function isInViewport(element) {
+    var rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+function animateElements() {
+    var slideLeftElements = document.querySelectorAll('.slide-left');
+    var slideRightElements = document.querySelectorAll('.slide-right');
+
+    slideLeftElements.forEach(function (element) {
+        if (isInViewport(element)) {
+            element.classList.add('fade-in');
+        }
+    });
+
+    slideRightElements.forEach(function (element) {
+        if (isInViewport(element)) {
+            element.classList.add('fade-in');
+        }
+    });
+}
+
+window.addEventListener('scroll', animateElements);
+window.addEventListener('resize', animateElements);
+window.addEventListener('load', animateElements);
